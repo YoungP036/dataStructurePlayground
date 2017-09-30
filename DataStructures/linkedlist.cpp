ea8dfad212ce8c3decd6b758f05ed4e94581157b
Patrick Young-ARCHVM <tua43500@temple.edu>
2017-09-30 14:33:14 -0400

    ll.cpp merged

--- DataStructures/linkedlist.cpp
+++ DataStructures/linkedlist.cpp
@@ -31,51 +31,17 @@ class linkedlist{
 	}
 	
 	bool search(int value){
-<<<<<<< HEAD
-		node *temp = new node;
-		temp=head;
-		//walk the list until hit end or find val
-		while(temp->data!=value&&temp!=NULL){
-			std::cout<<"looping in search\n";
-			temp=temp->next;
-		}
-		if(temp!=NULL) return true;
-		else return false;
-	}
-	
-	void remove(int value){
-		if(value!=(int)NULL){
-			node *temp = new node;
-			node *prev = NULL;
-			temp=head;
-			//find node to remove
-			while(temp->data!=value && temp->next!=NULL){
-				std::cout<<"while iterating\n";
-				prev=temp;
-				temp=temp->next;
-			}
-			//if temp=null the value is not in our list, if it is we remove
-			if(temp!=NULL){
-				//case 1: we are removing the only node in the list, thus prev=null
-				if(prev==NULL){
-					std::cout<<"Removing head node\n";
-					head=NULL;}
-				//case 2: prev exists, point it to the node after p
-				else{
-					std::cout<<"Removing deeper node\n";
-					prev->next=temp->next;}
-=======
-		if(head!=NULL){
-			node *temp = new node;
-			temp=head;
-			//walk the list until hit end or find val
-			while(temp->data!=value&&temp->next!=NULL){
-				temp=temp->next;
-			}
-			if(temp->data==value) return true;
-			else return false;
-			}
-		return false;
+			if(head!=NULL){
+				node *temp = new node;
+				temp=head;
+				//walk the list until hit end or find val
+				while(temp->data!=value&&temp->next!=NULL){
+					temp=temp->next;
+				}
+				if(temp->data==value) return true;
+				else return false;
+				}
+			return false;
 	}
 	
 	void remove(int value){
@@ -102,7 +68,6 @@ class linkedlist{
 						prev->next=temp->next;
 					}
 				}
->>>>>>> 131f879e308306e423ead30f73cfe082be45ab58
 			}
 			std::cout<<"exiting remove\n";
 		}
@@ -111,26 +76,35 @@ class linkedlist{
 	void printall(){
 		node* p = new node;
 		p=head;
-		std::cout<<"\n\nprinting list\n";
-		while(p!=NULL){
-			std::cout<<p->data<<"\n";
-			p=p->next;
-		}
-	}	
-};
+		if(p==NULL) std::cout<<"**List is empty**\n";
+		else{
+			std::cout<<"**printing list**\n";
+			while(p!=NULL){
+				std::cout<<p->data<<"\n";
+				p=p->next;
+			}//end while
+		}//end else
+	}//end print	
+};//end class
 
 int main(){
 	std::cout<<"compiled successfully\n";
 	linkedlist* list = new linkedlist();
 	list->insert(10);
 	list->insert(15);
-	if(list->search(15)==true) std::cout<<"15 search true, good\n";
-	if(list->search(20)==false) std::cout<<"20 search false, good\n";
-	else std::cout<<"20 search ret true, BAD\n";
-	list->remove(10);
-	list->remove(15);
+	if(list->search(15)==true) std::cout<<"15 search true, **GOOD\n";
+	else std::cout<<"15 search ret false, **BAD!\n";
+	if(list->search(20)==false) std::cout<<"20 search false, **GOOD\n";
+	else std::cout<<"20 search ret true, **BAD!\n";
+	std::cout<<"calling print on list {10,15}\n";
 	list->printall();
-	if(list->search(15)==false) std::cout<<"15 search ret false, good\n";
-	else std::cout<<"15 search ret true, bad!\n";
+	std::cout<<"calling remove(10)\n";
+	list->remove(10);
+	std::cout<<"calling remove(15)\n";
+	list->remove(15);
+	std::cout<<"calling printall on empty list\n";
+	list->printall();
+	if(list->search(15)==false) std::cout<<"15 search ret false, **GOOD\n";
+	else std::cout<<"15 search ret true, **BAD!\n";
 
 }
