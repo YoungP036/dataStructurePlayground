d66b2d7a10c884f48a1d0214edc7a12972442b47
Patrick Young <tua43500@temple.edu>
2017-09-29 23:25:05 -0400

    tested c++ linkedlist, added assembly HW

--- DataStructures/linkedlist.cpp
+++ DataStructures/linkedlist.cpp
@@ -29,35 +29,45 @@ class linkedlist{
 	}
 	
 	bool search(int value){
-		node *temp = new node;
-		temp=head;
-		//walk the list until hit end or find val
-		while(temp->data!=value&&temp!=NULL){
-			temp=temp->next;
-		}
-		if(temp!=NULL) return true;
-		else return false;
+		if(head!=NULL){
+			node *temp = new node;
+			temp=head;
+			//walk the list until hit end or find val
+			while(temp->data!=value&&temp->next!=NULL){
+				temp=temp->next;
+			}
+			if(temp->data==value) return true;
+			else return false;
+			}
+		return false;
 	}
 	
 	void remove(int value){
-		if(value!=(int)NULL){
-			node *temp = new node;
-			node *prev = new node;
-			temp=head;
-			//find node to remove
-			while(temp->data!=value && temp!=NULL){
-				prev=temp;
-				temp=temp->next;
+		if(head!=NULL){
+			if(value!=(int)NULL){
+				node *temp = new node;
+				node *prev=NULL;
+				temp=head;
+				//find node to remove
+				while(temp->data!=value && temp->next!=NULL){
+					prev=temp;
+					temp=temp->next;
+				}
+				//if temp=null the value is not in our list, if it is we remove
+				if(temp!=NULL){
+					//case 1: we are removing the only node in the list, thus prev=null
+					if(prev==NULL){
+						std::cout<<"removing head\n";
+						head=NULL;
+					}
+					//case 2: prev exists, point it to the node after p
+					else{
+						std::cout<<"removing not head node\n";
+						prev->next=temp->next;
+					}
+				}
 			}
-			//if temp=null the value is not in our list, if it is we remove
-			if(temp!=NULL){
-				//case 1: we are removing the only node in the list, thus prev=null
-				if(prev==NULL)
-					head=NULL;
-				//case 2: prev exists, point it to the node after p
-				else
-					prev->next=temp->next;
-			}
+			std::cout<<"exiting remove\n";
 		}
 	}
 	
@@ -65,7 +75,7 @@ class linkedlist{
 		node* p = new node;
 		p=head;
 		while(p!=NULL){
-			std::cout<<p->data<<"\t";
+			std::cout<<p->data<<"\n";
 			p=p->next;
 		}
 	}	
@@ -73,4 +83,15 @@ class linkedlist{
 
 int main(){
 	std::cout<<"compiled successfully\n";
+	linkedlist* list = new linkedlist();
+	list->insert(10);
+	list->insert(15);
+	if(list->search(15)==true) std::cout<<"15 search true, good\n";
+	if(list->search(20)==false) std::cout<<"20 search false, good\n";
+	else std::cout<<"20 search ret true, BAD\n";
+	list->remove(10);
+	list->remove(15);
+	list->printall();
+	if(list->search(15)==false) std::cout<<"15 search ret false, good\n";
+	else std::cout<<"15 search ret true, bad!\n";
 }
