fc5d3b982209420165ada8f9126128763cd85b54
fury <fury@Fury.fury>
2017-09-30 14:21:35 -0400

    restructure dirs

--- DataStructures/linkedlist.cpp
+++ DataStructures/linkedlist.cpp
@@ -33,6 +33,7 @@ class linkedlist{
 		temp=head;
 		//walk the list until hit end or find val
 		while(temp->data!=value&&temp!=NULL){
+			std::cout<<"looping in search\n";
 			temp=temp->next;
 		}
 		if(temp!=NULL) return true;
@@ -42,21 +43,24 @@ class linkedlist{
 	void remove(int value){
 		if(value!=(int)NULL){
 			node *temp = new node;
-			node *prev = new node;
+			node *prev = NULL;
 			temp=head;
 			//find node to remove
-			while(temp->data!=value && temp!=NULL){
+			while(temp->data!=value && temp->next!=NULL){
+				std::cout<<"while iterating\n";
 				prev=temp;
 				temp=temp->next;
 			}
 			//if temp=null the value is not in our list, if it is we remove
 			if(temp!=NULL){
 				//case 1: we are removing the only node in the list, thus prev=null
-				if(prev==NULL)
-					head=NULL;
+				if(prev==NULL){
+					std::cout<<"Removing head node\n";
+					head=NULL;}
 				//case 2: prev exists, point it to the node after p
-				else
-					prev->next=temp->next;
+				else{
+					std::cout<<"Removing deeper node\n";
+					prev->next=temp->next;}
 			}
 		}
 	}
@@ -64,6 +68,7 @@ class linkedlist{
 	void printall(){
 		node* p = new node;
 		p=head;
+		std::cout<<"\n\nprinting list\n";
 		while(p!=NULL){
 			std::cout<<p->data<<"\t";
 			p=p->next;
@@ -73,4 +78,19 @@ class linkedlist{
 
 int main(){
 	std::cout<<"compiled successfully\n";
+	linkedlist* list = new linkedlist();
+	list->insert(17);
+	list->printall();
+	list->remove(17);
+	list->printall();
+	list->insert(3);
+	list->insert(5);
+	list->insert(7);
+	list->insert(11);
+	list->insert(13);
+	list->printall();
+	if(list->search(7))
+		std::cout<<"saerch for 7- Found- Good\n";
+	if(!list->search(17))
+		std::cout<<"search for 17 not found- Good\n";
 }
